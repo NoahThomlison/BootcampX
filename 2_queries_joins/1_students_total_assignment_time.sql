dd2e2cfadb2744b063981547000c48f64bf02ab4
noahthomlison <noahthomlison@gmail.com>
2021-09-30 01:10:51 +0000

    selects total time for assignment for single student

---
+++ 2_queries_joins/1_students_total_assignment_time.sql
@@ -0,0 +1,5 @@
+SELECT SUM(assignment_submissions.duration)
+FROM students JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+WHERE students.name = 'Ibrahim Schimmel';
+
+-- -- \i 2_queries_joins/1_students_total_assignment_time.sql
