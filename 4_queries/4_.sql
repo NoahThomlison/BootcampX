eef4a467a39a3cbc35a3e5446384f55fa014efd0
noahthomlison <noahthomlison@gmail.com>
2021-10-01 03:39:43 +0000

    select the average assistance time

--- 4_queries/4_.sql
+++
@@ -1,7 +0,0 @@
-SELECT COUNT(assistance_requests.*), teachers.name
-FROM teachers
-JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id 
-WHERE teachers.name = 'Waylon Boehm'
-GROUP BY teachers.name;
-
--- \i 4_queries/1_total_teacher_requests.sql
---
+++ 4_queries/4_average_assistance_time.sql
@@ -0,0 +1,4 @@
+SELECT AVG(completed_at - started_at) as average_assistance_request_duration
+FROM assistance_requests
+
+-- \i 4_queries/4_average_assistance_time.sql
